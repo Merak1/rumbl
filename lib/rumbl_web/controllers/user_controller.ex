2ca0231f6b9f7879cc02e1f018f775cc7591576a
root <root@DESKTOP-DH9FK14.localdomain>
2023-03-08 23:32:41 -0600

    users can auth with a password taht is hashed and stored rpoperly on the db

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -14,20 +14,19 @@ defmodule RumblWeb.UserController do
     render(conn, "show.html", user: user)
   end
 
-  def new(conn, _params) do
-    changeset = Accounts.change_user(%User{})
-    render( conn, "new.html", changeset: changeset)
+def new(conn, _params) do
+  changeset = Accounts.change_registration(%User{}, %{})
+  render(conn, "new.html", changeset: changeset)
+end
+def create(conn, %{"user" => user_params}) do
+  case Accounts.register_user(user_params) do
+  {:ok, user} ->
+  conn
+    |> put_flash(:info, "#{user.name} created!")
+    |> redirect(to: Routes.user_path(conn, :index))
+  {:error, %Ecto.Changeset{} = changeset} ->
+    render(conn, "new.html", changeset: changeset)
   end
-
-  def create(conn, %{"user" => user_params}) do
-    case Accounts.create_user(user_params) do
-    {:ok, user} ->
-      conn
-      |> put_flash(:info, "#{user.name} created!")
-      |> redirect(to: Routes.user_path(conn, :index))
-    {:error, %Ecto.Changeset{} = changeset} ->
-      render(conn, "new.html", changeset: changeset )
-    end
-  end
+end
 
 end
